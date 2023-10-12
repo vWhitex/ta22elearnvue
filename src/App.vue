e8d1417b649eaf0fda44f0c25009197c2612695a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-12 11:24:45 +0300

    Add reverse name example

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-
+import { ref } from 'vue';
+const message = ref('Hello vue');
 </script>
 
 <template>
-
+  <button @click="message='Vue is awesome!'">Click me</button>
+  <input type="text" v-model="message">
+  <h1>{{ message.split('').reverse().join('') }}</h1>
 </template>
 
