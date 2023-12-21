813bb5e0c13ecfa18188b5d18376baa746897b50
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-12-21 10:54:37 +0200

    Add eslint

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,14 +1,15 @@
 <script setup>
-    import { useRouter, useRoute } from 'vue-router'
-    const route = useRoute();
-    const router = useRouter();
-    console.log(router.getRoutes());
+import { useRouter, useRoute } from 'vue-router';
+const route = useRoute();
+const router = useRouter();
+console.log(router.getRoutes());
 </script>
 
 <template>
     <div class="tabs is-centered">
         <ul>
-            <li v-for="link in router.getRoutes()" :class="{'is-active': route.path === link.path}">
+            <!-- eslint-disable-next-line vue/require-v-for-key -->
+            <li v-for="link in router.getRoutes()" :class="{ 'is-active': route.path === link.path }">
                 <router-link :to="link.path">{{ link.name }}</router-link>
             </li>
         </ul>
--- src/pages/ToDo.vue
+++ src/pages/ToDo.vue
@@ -4,43 +4,43 @@ import ItemList from '../components/ItemList.vue';
 const message = ref('');
 let i = 1;
 const items = reactive([
-  {id:i++,name:'leib', isDone: false },
-  {id:i++,name:'sai', isDone: true },
-  {id:i++,name:'piim', isDone: false },
-  {id:i++,name:'kommid', isDone: true },
+    { id: i++, name: 'leib', isDone: false },
+    { id: i++, name: 'sai', isDone: true },
+    { id: i++, name: 'piim', isDone: false },
+    { id: i++, name: 'kommid', isDone: true },
 ]);
 
 const doneItems = computed(() => {
-  return items.filter(item => item.isDone)
+    return items.filter(item => item.isDone);
 });
 
 const toDoItems = computed(() => {
-  return items.filter(item => !item.isDone)
+    return items.filter(item => !item.isDone);
 });
 
-function add(){
-  if(message.value.trim() !== ''){
-    items.push({id:i++, name: message.value.trim(), isDone: false});
-  }
-  message.value = '';
+function add() {
+    if (message.value.trim() !== '') {
+        items.push({ id: i++, name: message.value.trim(), isDone: false });
+    }
+    message.value = '';
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="field has-addons">
-      <p class="control is-expanded">
-        <input @keydown.enter="add" type="text" v-model="message" class="input">
-      </p>
-      <p class="control">
-        <button @click="add" class="button is-primary">Click me</button>
-      </p>
+    <div class="container">
+        <div class="field has-addons">
+            <p class="control is-expanded">
+                <input @keydown.enter="add" type="text" v-model="message" class="input">
+            </p>
+            <p class="control">
+                <button @click="add" class="button is-primary">Click me</button>
+            </p>
+        </div>
+        <div class="content">
+            <ItemList :items="items" title="All Items"></ItemList>
+            <ItemList :items="doneItems" title="Done Items"></ItemList>
+            <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
+        </div>
     </div>
-    <div class="content">
-      <ItemList :items="items" title="All Items"></ItemList>
-      <ItemList :items="doneItems" title="Done Items"></ItemList>
-      <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
-    </div>
-  </div>
 </template>
 
